edd6d070416ab473b0165d294709a436b88ebb1e
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-27 16:14:28 +0300

    Fix linter error

--- item.rb
+++ item.rb
@@ -1,19 +1,21 @@
 class Item
-    attr_accessor :genre, :author, :label, :publish_date, :archived
-    attr_reader :id
-  
-    def initialize(publish_date)
-      @id = Random.rand(1..1000)
-      @publish_date = publish_date
-      @archived = false
-      @label = label
-    end
-    def can_be_archived?
-        date = Date._parse(@publish_date)
-        publish_year = Date.today.year - date[:year]
-        publish_year > 10
-      end
-      def move_to_archive
-        @archived = can_be_archived?
-      end
+  attr_accessor :genre, :author, :label, :publish_date, :archived
+  attr_reader :id
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = false
+    @label = label
   end
+
+  def can_be_archived?
+    date = Date._parse(@publish_date)
+    publish_year = Date.today.year - date[:year]
+    publish_year > 10
+  end
+
+  def move_to_archive
+    @archived = can_be_archived?
+  end
+end
