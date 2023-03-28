f072516c6939ab1a39eba0141e1fc5d0f416919b
felixDev22 <felixnzomo@gmail.com>
2023-03-28 17:57:52 +0300

    Add test for generate Id

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,5 +7,11 @@ describe Genre do
       genre = Genre.new(name)
       expect(genre.name).to eq(name)
     end
+
+    it 'generates a random ID between 1 and 1000' do
+      name = 'Gospel'
+      genre = Genre.new(name)
+      expect(genre.instance_variable_get(:@id)).to be_between(1, 1000)
+    end
   end
 end
