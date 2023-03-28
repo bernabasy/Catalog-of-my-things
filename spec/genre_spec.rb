1a612bd75db82ecba6e815da65923423fbd91120
felixDev22 <felixnzomo@gmail.com>
2023-03-28 18:06:53 +0300

    Update genre_spec file, set genre to object

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -28,5 +28,12 @@ describe Genre do
       genre.add_item(album)
       expect(genre.items).to include(album)
     end
+
+    it "sets the item's genre to the genre object" do
+      genre = Genre.new('Rumba')
+      album = MusicAlbum.new('2022-01-01', true)
+      genre.add_item(album)
+      expect(album.genre).to eq(genre)
+    end
   end
 end
