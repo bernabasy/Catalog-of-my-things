694ba868d3bfdea521e39dfdc6c75a50efecc264
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-28 18:17:10 +0300

    fix linter error

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,25 +1,24 @@
 require_relative './spec_helper'
 
 RSpec.describe Label do
-    let(:label) { Label.new('Groceries', 'green') }
-  
-    describe '#initialize' do
-      it 'creates a new Label object' do
-        expect(label).to be_an_instance_of(Label)
-      end
-  
-      it 'sets title and color attributes' do
-        expect(label.title).to eq('Groceries')
-        expect(label.color).to eq('green')
-      end
-  
-      it 'generates a random id between 1 and 1000' do
-        expect(label.id).to be_between(1, 1000).inclusive
-      end
-  
-      it 'initializes an empty array of items' do
-        expect(label.instance_variable_get(:@items)).to be_empty
-      end
+  let(:label) { Label.new('Groceries', 'green') }
+
+  describe '#initialize' do
+    it 'creates a new Label object' do
+      expect(label).to be_an_instance_of(Label)
+    end
+
+    it 'sets title and color attributes' do
+      expect(label.title).to eq('Groceries')
+      expect(label.color).to eq('green')
+    end
+
+    it 'generates a random id between 1 and 1000' do
+      expect(label.id).to be_between(1, 1000).inclusive
+    end
+
+    it 'initializes an empty array of items' do
+      expect(label.instance_variable_get(:@items)).to be_empty
     end
   end
-  
+end
