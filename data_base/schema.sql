fe9ec8a2bcf75eda78bf8b29cfd5504e390542f6
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-28 18:49:20 +0300

    Create table for items, books, labels

---
+++ data_base/schema.sql
@@ -0,0 +1,28 @@
+-- Create Database
+CREATE DATABASE catalog;
+
+CREATE TABLE items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  source_id INT REFERENCES source(id),
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN
+);
+
+-- Create Books table
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR(250),
+  cover_state VARCHAR(250),
+  publish_date DATE,
+  archived BOOLEAN 
+);
+
+-- Create Labels tables
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(150),
+  color VARCHAR(150)
+);
