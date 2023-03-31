f703fafeaa07b7b594190ac2721b3c057b9978f4
felixDev22 <felixnzomo@gmail.com>
2023-03-31 13:52:52 +0300

    Update schema

--- data_base/schema.sql
+++ data_base/schema.sql
@@ -5,7 +5,6 @@ CREATE TABLE items (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   genre_id INT REFERENCES genre(id),
   author_id INT REFERENCES author(id),
-  source_id INT REFERENCES source(id),
   label_id INT REFERENCES label(id),
   publish_date DATE,
   archived BOOLEAN
