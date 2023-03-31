7e4065786753897faa5191f2b8ad8c94142a96aa
felixDev22 <felixnzomo@gmail.com>
2023-03-31 13:34:23 +0300

    Add table for genre and Music_album

--- data_base/schema.sql
+++ data_base/schema.sql
@@ -11,6 +11,20 @@ CREATE TABLE items (
   archived BOOLEAN
 );
 
+-- Create Genre table
+CREATE TABLE genre (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- Create Music Album table
+CREATE TABLE music_album (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN
+);
+
 -- Create Books table
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
